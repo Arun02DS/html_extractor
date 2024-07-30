b55567c6f47b8f32660578ef67eaf91577e20e73
Arun02DS <arun02negime@gmail.com>
2024-07-31 03:46:51 +0530

    file structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Html Extractor',
+    version= '0.0.0',
+    author= 'Arun Singh Negi',
+    author_email= 'arun02negime@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
